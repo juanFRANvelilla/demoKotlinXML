cc977e935fee3170705acd63a505e94a71377d45
juanfran perez <juanfrancisco.perez@grupocarreras.com>
2024-01-26 12:18:53 +0100

    ejemplos de lambdas con apply run...

--- app/src/main/java/com/example/demoxml/MainActivity.kt
+++ app/src/main/java/com/example/demoxml/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.example.demoxml.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), Loger{
@@ -32,9 +33,93 @@ class MainActivity : AppCompatActivity(), Loger{
 
 //        startActivity<MainActivity>()
 
-        toast(test())
+//        toast(test())
+
+
+        var person = Person("Alice", 30)
+
+        println("nombre original: ${person.name}")
+
+        /*
+        llamas a la funcion de extension creada para T (puede ser person) y ahi se hace uso de la
+        lambda que estas mandando, la lambda no devuelve nada pero la funcion devuelve el mismo tipo
+        que la llama
+         */
+        val resApply: Person = person.apply2 {
+            it.name = "cambio"
+        }
+
+        println("nombre de la clase despues de apply: ${person.name}")
+        println("nombre de la respuesta de apply: ${resApply.name}")
+
+        person = Person("Alice", 30)
+
+        println("\nnombre original: ${person.name}")
+
+        /*
+        aqui se llama a la funcion y se utiliza la lambda que se pasa, en este caso devuelve algo -> U
+        que seria en este caso un String, en la lambda se esta utilizando la info de la clase
+        que llama a la funcion
+         */
+        val resRun = person.run2 {
+            val newName = it.name + "nuevo"
+            "Nuevo nombrezzzxxx: $newName"
+        }
+        println("nombre de la clase despues de run: ${person.name}")
+        println("nombre de la respuesta de run: ${resRun}")
+
+        val resultado: String = person.let2 { persona ->
+            "Nombre: ${persona.name}, Edad: ${persona.age}"
+        }
+
+//        val resultado: String = Person("Alice", 30).let2 {
+//            "Nombre: ${name}, Edad: ${age}"
+//        }
+
+        toast(resApply.age.toString())
+
+//        print("Resultadoo $resultado")
+
     }
 }
+
+
+
+//fun <T> T.apply2(body: T.() -> Unit): T{
+//    this.body()
+//    return this
+//}
+
+fun <T> T.apply2(body: (T) -> Unit): T{
+    body(this)
+    return this
+}
+
+fun <T, U> T.run2(body: (T) -> U): U{
+    return body(this)
+}
+
+fun <T, U> T.run3(body: T.() -> U): U{
+    return this.body()
+}
+
+
+fun <T, U> T.let2(body: (T) -> U): U{
+    return body(this)
+//    return this.body()
+}
+
+fun <T, U> whith(reciver: T, body: T.() -> U): U{
+    return reciver.body()
+}
+
+fun <T> T.also2(body: (T) -> Unit): T{
+    body(this)
+    return this
+}
+
+class Person(var name:String, var age: Int)
+
 
 
 inline fun <reified T: Activity> Context.startActivity() {
--- app/src/main/java/com/example/demoxml/test.kt
+++ app/src/main/java/com/example/demoxml/test.kt
@@ -6,14 +6,22 @@ import android.view.ViewGroup
 import android.widget.TextView
 
 fun test(): String{
-    val sum: (Int, Int) -> Int = { x, y -> x + y}
-    val mul = {x: Int, y: Int -> x * y}
+//    val sum= { x: Int, y: Int -> x + y}
+    val sum: (Int, Int) -> Int = { x, y -> x + y }
+    val mul = {x: Int, y: Int -> x * y }
 
-    val res = doOp(2, 3, { x, y -> x - y })
+//    val res = doOp(2, 3, { x, y -> x - y })
+    val res = doOp(2, 3, sum)
     return res.toString()
 }
 
 fun doOp(x: Int, y: Int, op: (Int, Int) -> Int) = op(x,y)
+
+fun doOp2(x: Int,
+         y: Int,
+         op: (Int, Int) -> Int): Int {
+    return op(x,y)
+}
 
 
 interface Loger {
