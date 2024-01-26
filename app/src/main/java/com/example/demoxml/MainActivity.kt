ebbd9a5d08851fa4de43385285c314dd44a5e8bf
juanfran perez <juanfrancisco.perez@grupocarreras.com>
2024-01-26 18:31:52 +0100

    menu no funciona, demo de las lamdas apartadas y get item con for

--- app/src/main/java/com/example/demoxml/MainActivity.kt
+++ app/src/main/java/com/example/demoxml/MainActivity.kt
@@ -5,6 +5,8 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.example.demoxml.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), Loger{
@@ -31,95 +33,32 @@ class MainActivity : AppCompatActivity(), Loger{
 //        })
         adapter.items = getItems()
 
-
 //        startActivity<MainActivity>()
 
 //        toast(test())
 
 
-        var person = Person("Alice", 30)
 
-        println("nombre original: ${person.name}")
-
-        /*
-        llamas a la funcion de extension creada para T (puede ser person) y ahi se hace uso de la
-        lambda que estas mandando, la lambda no devuelve nada pero la funcion devuelve el mismo tipo
-        que la llama
-         */
-        val resApply: Person = person.apply2 {
-            it.name = "cambio"
-        }
-
-        println("nombre de la clase despues de apply: ${person.name}")
-        println("nombre de la respuesta de apply: ${resApply.name}")
-
-        person = Person("Alice", 30)
-
-        println("\nnombre original: ${person.name}")
-
-        /*
-        aqui se llama a la funcion y se utiliza la lambda que se pasa, en este caso devuelve algo -> U
-        que seria en este caso un String, en la lambda se esta utilizando la info de la clase
-        que llama a la funcion
-         */
-        val resRun = person.run2 {
-            val newName = it.name + "nuevo"
-            "Nuevo nombrezzzxxx: $newName"
-        }
-        println("nombre de la clase despues de run: ${person.name}")
-        println("nombre de la respuesta de run: ${resRun}")
-
-        val resultado: String = person.let2 { persona ->
-            "Nombre: ${persona.name}, Edad: ${persona.age}"
-        }
-
-//        val resultado: String = Person("Alice", 30).let2 {
-//            "Nombre: ${name}, Edad: ${age}"
-//        }
-
-        toast(resApply.age.toString())
-
-//        print("Resultadoo $resultado")
 
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        adapter.items = getItems().let {
+            when(item.itemId){
+                R.id.filter_photos-> it.filter { it.type == MediaItem.Type.PHOTO }
+                R.id.filter_videos -> it.filter { it.type == MediaItem.Type.VIDEO }
+                R.id.filter_all -> it
+                else -> emptyList()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
-
-
-
-//fun <T> T.apply2(body: T.() -> Unit): T{
-//    this.body()
-//    return this
-//}
-
-fun <T> T.apply2(body: (T) -> Unit): T{
-    body(this)
-    return this
-}
-
-fun <T, U> T.run2(body: (T) -> U): U{
-    return body(this)
-}
-
-fun <T, U> T.run3(body: T.() -> U): U{
-    return this.body()
-}
-
-
-fun <T, U> T.let2(body: (T) -> U): U{
-    return body(this)
-//    return this.body()
-}
-
-fun <T, U> whith(reciver: T, body: T.() -> U): U{
-    return reciver.body()
-}
-
-fun <T> T.also2(body: (T) -> Unit): T{
-    body(this)
-    return this
-}
-
-class Person(var name:String, var age: Int)
 
 
 
--- app/src/main/java/com/example/demoxml/getItems.kt
+++ app/src/main/java/com/example/demoxml/getItems.kt
@@ -2,15 +2,38 @@ package com.example.demoxml
 
 import com.example.demoxml.MediaItem.*
 
-fun getItems(): List<MediaItem> = listOf(
-    MediaItem("Title 1", "https://placekitten.com/200/200?image=1", Type.PHOTO),
-    MediaItem("Title 2", "https://placekitten.com/200/200?image=2", Type.PHOTO),
-    MediaItem("Title 3", "https://placekitten.com/200/200?image=3", Type.PHOTO),
-    MediaItem("Title 4", "https://placekitten.com/200/200?image=4", Type.VIDEO),
-    MediaItem("Title 5", "https://placekitten.com/200/200?image=5", Type.PHOTO),
-    MediaItem("Title 6", "https://placekitten.com/200/200?image=6", Type.PHOTO),
-    MediaItem("Title 7", "https://placekitten.com/200/200?image=7", Type.VIDEO),
-    MediaItem("Title 8", "https://placekitten.com/200/200?image=8", Type.PHOTO),
-    MediaItem("Title 9", "https://placekitten.com/200/200?image=9", Type.PHOTO),
-    MediaItem("Title 10", "https://placekitten.com/200/200?image=10", Type.PHOTO)
-)
+//fun getItems(): List<MediaItem> {
+//    var items: MutableList<MediaItem> = mutableListOf()
+//    for(i in 1..10){
+//        val name: String = "Title $i"
+//        val url: String = "https://placekitten.com/200/200?image=$i"
+//        val type: Type = when(i % 3){
+//            0-> Type.VIDEO
+//            else -> Type.PHOTO
+//        }
+//        items.add(MediaItem(name,url,type))
+//
+//    }
+//    return items.toList()
+//}
+
+fun getItems(): List<MediaItem> = (1..10).map {
+    MediaItem("Title $it", "https://placekitten.com/200/200?image=$it", when(it % 3){ 0-> Type.VIDEO else -> Type.PHOTO})
+}
+
+
+
+
+
+
+
+//    MediaItem("Title 1", "https://placekitten.com/200/200?image=1", Type.PHOTO),
+//    MediaItem("Title 2", "https://placekitten.com/200/200?image=2", Type.PHOTO),
+//    MediaItem("Title 3", "https://placekitten.com/200/200?image=3", Type.PHOTO),
+//    MediaItem("Title 4", "https://placekitten.com/200/200?image=4", Type.VIDEO),
+//    MediaItem("Title 5", "https://placekitten.com/200/200?image=5", Type.PHOTO),
+//    MediaItem("Title 6", "https://placekitten.com/200/200?image=6", Type.PHOTO),
+//    MediaItem("Title 7", "https://placekitten.com/200/200?image=7", Type.VIDEO),
+//    MediaItem("Title 8", "https://placekitten.com/200/200?image=8", Type.PHOTO),
+//    MediaItem("Title 9", "https://placekitten.com/200/200?image=9", Type.PHOTO),
+//    MediaItem("Title 10", "https://placekitten.com/200/200?image=10", Type.PHOTO)
