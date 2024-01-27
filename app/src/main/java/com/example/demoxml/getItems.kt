fee88b1e9a073c621e3ec6c34392f88bece6baeb
juanFRANvelilla <juanfran01velilla@gmail.com>
2024-01-27 12:13:22 +0100

    crear object para getitems

--- app/src/main/java/com/example/demoxml/getItems.kt
+++ app/src/main/java/com/example/demoxml/getItems.kt
@@ -2,6 +2,13 @@ package com.example.demoxml
 
 import com.example.demoxml.MediaItem.*
 
+object MediaProvider {
+    fun getItems(): List<MediaItem> = (1..10).map {
+        MediaItem("Title $it", "https://placekitten.com/200/200?image=$it", when(it % 3){ 0-> Type.VIDEO else -> Type.PHOTO})
+    }
+
+}
+
 //fun getItems(): List<MediaItem> {
 //    var items: MutableList<MediaItem> = mutableListOf()
 //    for(i in 1..10){
@@ -17,23 +24,7 @@ import com.example.demoxml.MediaItem.*
 //    return items.toList()
 //}
 
-fun getItems(): List<MediaItem> = (1..10).map {
-    MediaItem("Title $it", "https://placekitten.com/200/200?image=$it", when(it % 3){ 0-> Type.VIDEO else -> Type.PHOTO})
-}
 
 
 
 
-
-
-
-//    MediaItem("Title 1", "https://placekitten.com/200/200?image=1", Type.PHOTO),
-//    MediaItem("Title 2", "https://placekitten.com/200/200?image=2", Type.PHOTO),
-//    MediaItem("Title 3", "https://placekitten.com/200/200?image=3", Type.PHOTO),
-//    MediaItem("Title 4", "https://placekitten.com/200/200?image=4", Type.VIDEO),
-//    MediaItem("Title 5", "https://placekitten.com/200/200?image=5", Type.PHOTO),
-//    MediaItem("Title 6", "https://placekitten.com/200/200?image=6", Type.PHOTO),
-//    MediaItem("Title 7", "https://placekitten.com/200/200?image=7", Type.VIDEO),
-//    MediaItem("Title 8", "https://placekitten.com/200/200?image=8", Type.PHOTO),
-//    MediaItem("Title 9", "https://placekitten.com/200/200?image=9", Type.PHOTO),
-//    MediaItem("Title 10", "https://placekitten.com/200/200?image=10", Type.PHOTO)
